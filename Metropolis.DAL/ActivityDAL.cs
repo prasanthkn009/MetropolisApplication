2737101cdd205a24df4e86824ee75a3f089f0463
Anu Maria <anuanniesunny@gmail.com>
2020-12-06 00:01:27 +0530

    StreetBLL

---
+++ Metropolis.DAL/ActivityDAL.cs
@@ -0,0 +1,27 @@
+﻿using Metropolis.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Metropolis.DAL
+{
+    public class ActivityDAL
+    {
+        private readonly ApplicationDbContext _dbcontext;
+       
+        public ActivityDAL(ApplicationDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+
+        public List<Activity> GetActivities(DateTime fromDate, DateTime toDate)
+        {
+            return _dbcontext.Activities
+            .Where(x => x.ScheduledDate >= fromDate && x.ScheduledDate <= toDate)
+            .ToList();
+        }
+
+    }
+}
+
---
+++ Metropolis.UI/Controllers/ActivityController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metropolis.UI.Controllers
+{
+    public class ActivityController : Controller
+    {
+        private readonly IActivityBLL _activityBLL;
+
+        public ActivityController (IActivityBLL activityBLL)
+        {
+            _activityBLL = activityBLL;
+
+        }
+        public IActionResult Index()
+        {
+            var activities = _activityBLL.GetActivitiesForTheDay();
+            activities.select(x=> new ActivityViewModel
+            {
+                ScheduledDate = x.ScheduledDate.ToString()
+            });
+            return View();
+        }
+    }
+}
