35db2820d548a3f67f0ce93f3f662136d205df8b
Athul Sanjeevan <athulsanjeevan12@gmail.com>
2020-12-07 10:22:18 +0530

    merge conflict managed

--- Metropolis.BLL/AddBLL.cs
+++ Metropolis.BLL/AddBLL.cs
@@ -22,21 +22,7 @@ namespace Metropolis.BLL
             string Errmsg;
             int Total = data.Where(x => x.ScheduledDate == New_data.ScheduledDate).Count();
             int count = data.Where(x => x.IsClosed== New_data.IsClosed).Count(); 
-<<<<<<< HEAD
-           
-=======
-            foreach (List<Activity>Element in data)
-            {
-                if (Element.ActivityName.where(x => x.ScheduledDate == New_data.ScheduledDate) == New_data.ActivityName) 
-                {
-                    Errmsg = "Activity with same name already exist for the date";
-                    Flag = 0;
-                    break;
-                }
-                else { Flag = 1; }
-            }
->>>>>>> 9f75f09d7e8ba81d9a1415df344745a33784e087
-            if (Total < 15)
+            if (Total < 15) 
             {
                 if (New_data.IsClosed == true)
                 {
