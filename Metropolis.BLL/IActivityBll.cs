2d869fc400dd24b4022ea628ca5c635ff1fd1a65
Athulsanjeevan <74927688+Athulsanjeevan@users.noreply.github.com>
2020-12-14 14:08:26 +0530

    Update IActivityBll.cs

--- Metropolis.BLL/IActivityBll.cs
+++ Metropolis.BLL/IActivityBll.cs
@@ -8,12 +8,7 @@ namespace Metropolis.BLL
     /// </summary>
     public interface IActivityBll
     {
-<<<<<<< HEAD
         void Add(Activity newdata);
-=======
-   
-        void AddToDatabase(Activity newData);
->>>>>>> 85b0c5b2f9532f44c5bd0fb7e9e066e5e8079720
         void Delete(int id);
         List<Activity> GetActivitiesForTheDay();
         void Update(Activity newdata, int id);
